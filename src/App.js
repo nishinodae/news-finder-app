1bcb1ff2f3cc43d62534d43d0c2dad6d88c7b2f1
nishinodae <masyitahsalwa@gmail.com>
2025-11-13 20:24:57 +0800

    added Suspense

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { NewsContextProvider } from './context/NewsContext';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './utils/theme';
-import { lazy } from 'react';
+import { lazy, Suspense } from 'react';
+import { LinearProgress } from '@mui/material';
 
 const Login = lazy(() => import('./components/Login'));
 const Home = lazy(() => import('./components/Home'));
 
 function App() {
   return (
-    <><ThemeProvider theme={theme}>
-        <Router>
-          <NewsContextProvider>
-            <Routes>
-              <Route path='/' element={<Login />} />
-              <Route path='/home' element={<Home />} />
-            </Routes>
-          </NewsContextProvider>
-        </Router>
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <Suspense fallback={<LinearProgress />}>
+          <Router>
+            <NewsContextProvider>
+              <Routes>
+                <Route path='/' element={<Login />} />
+                <Route path='/home' element={<Home />} />
+              </Routes>
+            </NewsContextProvider>
+          </Router>
+        </Suspense>
+      </ThemeProvider>
     </>
   );
 }
