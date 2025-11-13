c83b41100afc9bfdc0dadbf87ddfc84aebcde655
nishinodae <masyitahsalwa@gmail.com>
2025-11-13 20:43:04 +0800

    separate typingValue from searchTerm to avoid heavy re-rendering

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,19 +4,19 @@ import MyFavPanel from './MyFavPanel';
 import CustomSnackbar from './CustomSnackbar';
 import { useNewsContext } from '../context/NewsContext';
 import { Navigate } from 'react-router-dom';
-import { lazy } from 'react';
-
-const DisplayResults = lazy(() => import('./DisplayResults'));
+import DisplayResults from './DisplayResults';
+import { useState } from 'react';
 
 const Home = () => {
     const { user, fav, news } = useNewsContext();
+    const [typingValue, setTypingValue] = useState('');
 
     return user === '' ? <Navigate to='/'></Navigate> : (
         <Grid container direction='column'>
-            <Header />
+            <Header typingValue={typingValue} setTypingValue={setTypingValue} />
             <Grid container sx={{ p: '0 20px' }} spacing={3}>
                 {fav.length !== 0 && <MyFavPanel />}
-                {news.length === 0 ? <CustomSnackbar /> : <DisplayResults />}
+                {news.length === 0 ? <CustomSnackbar typingValue={typingValue} /> : <DisplayResults />}
             </Grid>
         </Grid>
     );
