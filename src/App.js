209c8c6aa62ece7cb35d579fe2ef521dc763a733
nishinodae <masyitahsalwa@gmail.com>
2025-11-13 11:33:22 +0800

    lazy import Login and Home

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import Home from './components/Home';
 import { NewsContextProvider } from './context/NewsContext';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './utils/theme';
+import { lazy } from 'react';
+
+const Login = lazy(() => import('./components/Login'));
+const Home = lazy(() => import('./components/Home'));
 
 function App() {
   return (
     <><ThemeProvider theme={theme}>
-      <Router>
-        <NewsContextProvider>
-          <Routes>
-            <Route path='/' element={<Login />} />
-            <Route path='/home' element={<Home />} />
-          </Routes>
-        </NewsContextProvider>
-      </Router>
+        <Router>
+          <NewsContextProvider>
+            <Routes>
+              <Route path='/' element={<Login />} />
+              <Route path='/home' element={<Home />} />
+            </Routes>
+          </NewsContextProvider>
+        </Router>
     </ThemeProvider>
     </>
   );
