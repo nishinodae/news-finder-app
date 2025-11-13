a2addec563e1b8bad3c7c186aef996b2b95a219d
nishinodae <masyitahsalwa@gmail.com>
2025-11-13 11:38:47 +0800

    {news.length === 0 ? <CustomSnackbar /> : <DisplayResults />}

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,25 @@
 import { Grid } from '@mui/material';
 import Header from './Header';
-import DisplayResults from './DisplayResults';
 import MyFavPanel from './MyFavPanel';
+import CustomSnackbar from './CustomSnackbar';
 import { useNewsContext } from '../context/NewsContext';
 import { Navigate } from 'react-router-dom';
+import { lazy } from 'react';
+
+const DisplayResults = lazy(() => import('./DisplayResults'));
 
 const Home = () => {
-    const { user, fav } = useNewsContext();
-    return user === '' ?
-        <Navigate to='/'></Navigate> : (
-            <Grid container direction='column'>
-                <Header />
-                <Grid container sx={{ p: '0 20px' }} spacing={3}>
-                    {fav.length !== 0 && <MyFavPanel />}
-                    <DisplayResults />
-                </Grid>
+    const { user, fav, news } = useNewsContext();
+
+    return user === '' ? <Navigate to='/'></Navigate> : (
+        <Grid container direction='column'>
+            <Header />
+            <Grid container sx={{ p: '0 20px' }} spacing={3}>
+                {fav.length !== 0 && <MyFavPanel />}
+                {news.length === 0 ? <CustomSnackbar /> : <DisplayResults />}
             </Grid>
-        );
+        </Grid>
+    );
 };
 
 export default Home;
